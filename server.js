70dcc1d8f61101b576efb53b955a4f1edbf7fa79
Ikram <ikramrashid1@googlemail.com>
2024-08-18 17:03:12 +0100

    feat: implement PUT request

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const generateUid = require('./helper-functions')
 const PORT = 3000;
 
 let totalBudget = 1000;
-let envelopes = [];
+let budgetRem = totalBudget;
+let envelopes = [{
+    id: 12,
+    name: "Groceries",
+    amount: 50
+}, {
+    id: 15,
+    name: "Fuel",
+    amount: 150
+
+}];
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -19,8 +29,8 @@ app.get('/envelopes', (req, res, next) => {
     res.json(envelopes)
 })
 
-app.get('/envelopes/:userId', (req, res, next) => {
-    let id = Number(req.params.userId);
+app.get('/envelopes/:id', (req, res, next) => {
+    let id = Number(req.params.id);
     console.log(`Searching for envelope with id ${id}...`);
 
     let envelope = envelopes.find(element => element.id === id);
@@ -42,8 +52,8 @@ app.post('/envelopes', (req, res, next) => {
         return res.status(400).send("Error: invalid arguments for envelope");
     }
 
-    if (totalBudget - amount < 0) {
-        console.log("Not enough funds to create envelope");
+    if (budgetRem - amount < 0) {
+        console.log("Not enough funds remaining");
         return res.status(400).send("Error: not enough funds");
 
     }
@@ -54,11 +64,34 @@ app.post('/envelopes', (req, res, next) => {
             name,
             amount
         });
-    totalBudget -= amount; //deduct amount from total budget
+    budgetRem -= amount; //deduct amount from total budget
 
-    console.log(`Envelope created successfully... total budget is now ${totalBudget}`);
-    res.status(200).send("Envelope created successfully")
+    console.log(`Envelope created successfully`);
+    res.status(200).send("Envelope created successfully");
+})
 
+app.put('/envelopes/:id', (req, res, next) => {
+    const id = Number(req.params.id);
+    console.log(`Searching for envelope with id ${id}...`);
+    const envelope = envelopes.find(element => element.id === id);
+    const { name, amount } = req.body;
+
+    if (!envelope) {
+        return res.status(404).send('Error: Envelope not found');
+    }
+
+    let budgetCost = envelope.amount - amount;
+
+    console.log(`Budget will cost ${budgetCost}...`);
+
+    // Update the envelope's properties
+    if (name) envelope.name = name;
+    if (amount) envelope.amount = amount;
+
+    totalBudget -= budgetCost;
+    console.log(`Envelope updated... total budget is now ${totalBudget}`);
+
+    res.send(envelope);
 })
 
 app.listen(PORT, () => {
